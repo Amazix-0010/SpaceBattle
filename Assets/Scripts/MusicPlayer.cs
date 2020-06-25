92321b65b54299842ca1e6334fc8ce00f60220e3
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-06-25 10:08:08 +0200

    MusicPlayer

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -8,7 +8,7 @@ public class MusicPlayer : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Invoke("LoadFirstScene", 2f);
+        Invoke("LoadFirstScene", 5f);
     }
 
     // Update is called once per frame
