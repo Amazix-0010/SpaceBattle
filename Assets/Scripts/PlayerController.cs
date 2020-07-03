fa8fe270f76605ea8cd3b49be79ae9bca43699e2
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-07-03 20:11:42 +0200

    Rotation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,10 +8,16 @@ using UnityStandardAssets.CrossPlatformInput;
 public class PlayerController : MonoBehaviour
 {
     [Tooltip("In m/s - x")][SerializeField] float moveSpeed = 50f;
-    [Tooltip("In m - x")][SerializeField] float xRange = 17f;
+    [Tooltip("In m - x")][SerializeField] float xRange = 19f;
 
-    [Tooltip("In m - y")][SerializeField] float yRangeMax = 10f;
-    [Tooltip("In m - y")][SerializeField] float yRangeMin = -10f;
+    [Tooltip("In m - y")][SerializeField] float yRangeMax = 12f;
+    [Tooltip("In m - y")][SerializeField] float yRangeMin = -12f;
+
+    [SerializeField] float positionPitchFactor = -1f;
+    [SerializeField] float controlPitchFactor = -30f;
+
+
+    float xThrow, yThrow;
 
     // Start is called before the first frame update
     void Start()
@@ -28,20 +34,25 @@ public class PlayerController : MonoBehaviour
 
     private void Rotation()
     {
-        transform.localRotation = Quaternion.Euler(0f, 30f, 0f);
+        float pitchDueToPosition = transform.localPosition.y * positionPitchFactor;
+        float PitchDueToControlThrow = yThrow * controlPitchFactor;
+        float pitch = pitchDueToPosition + PitchDueToControlThrow;
+        float yaw = 0f;
+        float roll = 0f;
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
     }
 
     private void Translation()
     {
         // X
-        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * moveSpeed * Time.deltaTime;
 
         float rawPosX = transform.localPosition.x + xOffset;
         float clampedXPos = Mathf.Clamp(rawPosX, -xRange, xRange);
 
         // Y
-        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        yThrow = CrossPlatformInputManager.GetAxis("Vertical");
         float yOffset = yThrow * moveSpeed * Time.deltaTime;
 
         float rawPosY = transform.localPosition.y + yOffset;
