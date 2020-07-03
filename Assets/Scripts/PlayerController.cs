519f69502e8a54c08df8384d2835fdad28819fab
Amazix-0010 <65762218+Amazix-0010@users.noreply.github.com>
2020-07-03 17:42:28 +0200

    Controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class PlayerController : MonoBehaviour
+{
+    [Tooltip("In m/s")][SerializeField] float xSpeed = 50f;
+    [Tooltip("In m")] [SerializeField] float xRange = 17f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+        float rawPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawPos, -xRange, xRange);
+
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
